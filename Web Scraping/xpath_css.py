299c32e53a09ed2a1daab5c65755778f7241f004
felicitytech <souceking@gmail.com>
2024-11-21 16:51:40 +0100

    The(X)path to CSS Locators

---
+++ Web Scraping/xpath_css.py
@@ -0,0 +1,5 @@
+# Create the XPath string equivalent to the CSS Locator 
+xpath = '//div[@id="uid"]/span//h4'
+
+# Create the CSS Locator string equivalent to the XPath
+css_locator = 'div#uid > span h4'
